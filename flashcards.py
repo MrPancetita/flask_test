09ceeb4c9d8859236087622a472bb378361ede46
MrPancetita <contreras.engineer@outlook.com>
2022-06-17 17:46:30 +0200

    First version

--- flashcards.py
+++ flashcards.py
@@ -1,9 +1,70 @@
-from flask import Flask
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+
+from model import db, save_db
 
 app = Flask(__name__)
+counter = 0
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Welcome to my Flash Cards application!'
+def welcome():
+    return render_template(
+        "welcome.html",
+        cards=db
+    )
+
+
+@app.route('/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        # from has been submitted, process data
+        card = {"question": request.form['question'], "answer": request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:
+        return render_template(
+            "add_card.html"
+        )
+
+@app.route('/card/<int:index>')
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template(
+            "card.html",
+            card=card,
+            index=index,
+            max_index=len(db) - 1
+        )
+    except IndexError:
+        abort(404)
+
+@app.route('/remove_card/<int:index>', methods=["GET", "POST"])
+def remove_card(index):
+    try:
+        card = db[index]
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome'))
+        else:
+            return render_template(
+                "remove_card.html",
+                card=card
+            )
+    except IndexError:
+        abort(404)
+
+@app.route('/api/card/')
+def api_card_list():
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
 
---
+++ model.py
@@ -0,0 +1,16 @@
+""""
+Demo stuff. Don't take it seriously.
+"""
+
+
+import json
+
+def load_db():
+    with open("flashcards_db.json") as f:
+        return json.load(f)
+
+def save_db():
+    with open("flashcards_db.json", 'w') as f:
+        return json.dump(db,f)
+
+db = load_db()
